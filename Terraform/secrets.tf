e862596a724ed953ae457c3ddd7649546a1c3fec
Utkarsh0438 <ukmishra0438@gmail.com>
2023-05-22 12:30:47 +0530

    file remove jk

--- Terraform/secrets.tf
+++ Terraform/secrets.tf
@@ -1,6 +1,6 @@
 
 resource "aws_secretsmanager_secret" "simple-web-app" {
-  name = "simple-web-app-1a"
+  name = "simple-web-app-1b"
 }
 
 resource "aws_secretsmanager_secret_version" "simple-web-app" {
